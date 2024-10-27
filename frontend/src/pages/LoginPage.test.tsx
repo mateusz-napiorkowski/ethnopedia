cf500f6998c592591b0cb8db64879639e56d5639
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2024-10-27 15:27:44 +0100

    install and make jest work on frontend

---
+++ frontend/src/pages/LoginPage.test.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { render, screen } from '@testing-library/react'
+import {describe, expect, test, jest, beforeEach} from "@jest/globals"
+import LoginPage from "./LoginPage"
+
+test('example test', () => {
+    // render(<LoginPage />)
+    expect(2).toBe(2)
+});
