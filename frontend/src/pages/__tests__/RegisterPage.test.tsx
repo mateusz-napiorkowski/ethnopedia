78b7b75c1431133f964003fecfe22be599d0a364
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2025-01-10 20:52:06 +0100

    refactor and finish tests for registerPage component

--- frontend/src/pages/__tests__/RegisterPage.test.tsx
+++ frontend/src/pages/__tests__/RegisterPage.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react'
+import { render, waitForElementToBeRemoved } from '@testing-library/react'
 import RegisterPage from "../RegisterPage"
 import { MemoryRouter, Routes, Route } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
@@ -11,8 +11,23 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockUseNavigate,
 }));
 
+interface FormValues {
+    username: string,
+    firstName: string,
+    password: string
+}
+
+const mockRegisterUser = jest.fn()
+jest.mock('../../api/auth', () => ({
+    registerUser: ({ username, firstName, password }: FormValues) => mockRegisterUser({ username, firstName, password })
+}))
+
 const queryClient = new QueryClient();
 const user = userEvent.setup()
+
+const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3Rvd3kiLCJmaXJzdE5hbWUiOiJ0ZXN0b3d5IiwidXN"
+    + "lcklkIjoiNjZiNjUwNmZiYjY0ZGYxNjVlOGE5Y2U2IiwiaWF0IjoxNzI0MTg0MTE0LCJleHAiOjE3MjUxODQxMTR9.fzHPaXFMzQTVUf9IdZ0G6oeiaecc"
+    + "N-rDSjRS3kApqlA"
 
 const renderComponent = () => {
     return render(
@@ -98,13 +113,12 @@ describe("RegisterPage tests", () => {
         },
       ])('$testName', async ({fieldLabels, valuesToTypeIn, errorMessages, showErrorMessages}) => {
         const {getByLabelText, getByRole, queryByText } = renderComponent()
+        const registerButton = getByRole("button", {name: /zarejestruj się/i})
 
         for (const [fieldIndex, fieldLabel] of fieldLabels.entries()) {
             const fieldElement = getByLabelText(fieldLabel);
             await user.type(fieldElement, valuesToTypeIn[fieldIndex]);
         }
-
-        const registerButton = getByRole("button", {name: /zarejestruj się/i})
         await user.click(registerButton)
 
         errorMessages.forEach(async (message, index) => {
@@ -114,5 +128,74 @@ describe("RegisterPage tests", () => {
                 expect(queryByText(message)).not.toBeInTheDocument()
             }
         });
+    })
+
+    it("should show error toast when user already exists, registerUser should be called with correct arguments, toast should disappear after a few seconds", async () => {
+        mockRegisterUser.mockImplementation(() => {throw Error("Mocked registerUser error")})          
+        const { getByLabelText, getByRole, getByText, queryByText } = renderComponent()
+        const firstNameField = getByLabelText("Imię");
+        const usernameField = getByLabelText("Nazwa użytkownika");
+        const passwordField = getByLabelText("Hasło");
+        const confirmPasswordField = getByLabelText("Powtórz hasło");
+        const registerButton = getByRole("button", {name: /zarejestruj się/i})
+
+        await user.type(firstNameField, "Jacek");
+        await user.type(usernameField, "ExistingUsername");
+        await user.type(passwordField, "haslojacka");
+        await user.type(confirmPasswordField, "haslojacka");
+        await user.click(registerButton)
+
+        expect(mockRegisterUser).toHaveBeenCalledWith({
+            username: "ExistingUsername",
+            firstName: "Jacek",
+            password: "haslojacka"
+        })
+        expect(getByText("Użytkownik już istnieje.")).toBeInTheDocument()
+        await waitForElementToBeRemoved(() => queryByText("Użytkownik już istnieje."), {timeout: 5000})
+        expect(queryByText("Użytkownik już istnieje.")).not.toBeInTheDocument()
+    })
+
+    it("should hide error toast when user clicks the 'X' button on it", async () => {
+        mockRegisterUser.mockImplementation(() => {throw Error("Mocked registerUser error")})          
+        const { getByLabelText, getByRole, queryByText } = renderComponent()
+        const firstNameField = getByLabelText("Imię");
+        const usernameField = getByLabelText("Nazwa użytkownika");
+        const passwordField = getByLabelText("Hasło");
+        const confirmPasswordField = getByLabelText("Powtórz hasło");
+        const registerButton = getByRole("button", {name: /zarejestruj się/i})
+
+        await user.type(firstNameField, "Jacek");
+        await user.type(usernameField, "ExistingUsername");
+        await user.type(passwordField, "haslojacka");
+        await user.type(confirmPasswordField, "haslojacka");
+        await user.click(registerButton)
+        await user.click(getByLabelText("Close"))
+
+        expect(queryByText("Użytkownik już istnieje.")).not.toBeInTheDocument()
+    })
+
+    it("should call registerUser with correct arguments, useNavigate should be called with a path to home page", async () => {
+        mockRegisterUser.mockReturnValue({data: {
+            "token": jwtToken
+        }})          
+        const { getByLabelText, getByRole } = renderComponent()
+        const firstNameField = getByLabelText("Imię");
+        const usernameField = getByLabelText("Nazwa użytkownika");
+        const passwordField = getByLabelText("Hasło");
+        const confirmPasswordField = getByLabelText("Powtórz hasło");
+        const registerButton = getByRole("button", {name: /zarejestruj się/i})
+
+        await user.type(firstNameField, "Jacek");
+        await user.type(usernameField, "jacek123");
+        await user.type(passwordField, "haslojacka");
+        await user.type(confirmPasswordField, "haslojacka");
+        await user.click(registerButton)
+
+        expect(mockRegisterUser).toHaveBeenCalledWith({
+            username: "jacek123",
+            firstName: "Jacek",
+            password: "haslojacka"
+        })
+        expect(mockUseNavigate).toHaveBeenCalledWith("/")
     })
 })
