5d56bef790be87519ce3ff0951a8b08f07595ecf
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2025-04-26 22:22:29 +0200

    fix eslist errors

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import ToggleTheme from "./ToggleTheme"
 import { ReactComponent as UserIcon } from "../../assets/icons/user-icon.svg"
 import { deleteAccount } from "../../api/auth"
