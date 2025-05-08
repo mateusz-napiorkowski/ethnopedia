689d3c7245669b1bd30ecb95c97bbe369070dc4d
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2025-05-08 10:53:13 +0200

    add tests for SortOptions component

--- frontend/src/components/__tests__/SortOptions.test.tsx
+++ frontend/src/components/__tests__/SortOptions.test.tsx
@@ -2,24 +2,32 @@ import '@testing-library/jest-dom';
 import { render } from '@testing-library/react'
 import SortOptions from "../SortOptions"
 import userEvent from "@testing-library/user-event";
+import { useState } from 'react';
 
-const mockOnSelect = jest.fn()
 const mockSelectCurrentPage = jest.fn()
 
 const user = userEvent.setup()
 
 const renderComponent = () => {
-    return render(
+    return render(<SortOptionsWrapper/>)
+}
+
+const SortOptionsWrapper = () => {
+    const [sortCategory, setSortCategory] = useState("Tytuł")
+    const [sortDirection, setSortDirection] = useState("asc")
+    return (
         <SortOptions 
             options={[
-                {value: "name-asc", label: "Nazwa rosnąco"},
-                {value: "name-desc", label: "Nazwa malejąco"}
+                {value: "Tytuł", label: "Tytuł"},
+                {value: "Tytuł.Podtytuł", label: "Tytuł.Podtytuł"},
+                {value: "Data nagrania", label: "Data nagrania"}
             ]}
-            onSelect={mockOnSelect}
-            sortOrder='name-asc'
+            sortCategory={sortCategory}
+            sortDirection={sortDirection}
+            onSelectCategory={setSortCategory}
+            onSelectDirection={setSortDirection}
             setCurrentPage={mockSelectCurrentPage}
-        />
-    );
+        />)
 };
 
 describe("Sort options tests", () => {
@@ -27,27 +35,56 @@ describe("Sort options tests", () => {
         jest.clearAllMocks()
     });
 
-    it("should render component correctly", () => {           
+    it("should render initial state", () => {           
         const {container} = renderComponent()
         
         expect(container).toMatchSnapshot()
     })
 
-    it("should call onSelect, change selected option in select menu and call setCurrentPage(1) after user chooses an option from select menu", async () => {           
-        const { getByRole } = renderComponent()
-        const selectElement = getByRole("combobox")
-        const nameAscOption = getByRole("option", {
-            name: /nazwa malejąco/i
-        })
-        const nameDescOption = getByRole("option", {
-            name: /nazwa rosnąco/i
-        })
+    it("should open dropdown menu when it is clicked", async () => {           
+        const {container, getByLabelText} = renderComponent()
+        const categoriesDropdownMenu = getByLabelText("sortby-category-dropdown")
 
-        await user.selectOptions(selectElement, "name-desc")
+        await user.click(categoriesDropdownMenu)
 
-        expect(mockOnSelect).toHaveBeenCalled()
-        expect((nameAscOption as HTMLOptionElement).selected).toBe(true)
-        expect((nameDescOption as HTMLOptionElement).selected).toBe(false)
-        expect(mockSelectCurrentPage).toHaveBeenCalledWith(1) 
+        expect(container).toMatchSnapshot()
+    })
+
+    it("should change selected category when option from dropdown menu when clicked, should call selectCurrentPage(1)", async () => {           
+        const {container, getByLabelText} = renderComponent()
+        const categoriesDropdownMenu = getByLabelText("sortby-category-dropdown")
+
+        await user.click(categoriesDropdownMenu)
+
+        const optionToSelect = getByLabelText("Data nagrania")
+
+        await user.click(optionToSelect)
+
+        expect(container).toMatchSnapshot()
+        expect(mockSelectCurrentPage).toHaveBeenCalledWith(1)
+    })
+
+    it("should rotate sorting direction button after sorting direction button is clicked and change its title to 'Sortuj rosnąco', should call selectCurrentPage(1)", async () => {          
+        const {container, getByLabelText} = renderComponent()
+        const sortingDirectionButton = getByLabelText("Zmień kierunek sortowania")
+
+        await user.click(sortingDirectionButton)
+        
+        expect(container).toMatchSnapshot()
+        expect(mockSelectCurrentPage).toHaveBeenCalledWith(1)
+    })
+
+    it("should rotate sorting direction button after sorting direction button is clicked and change its title to 'Sortuj malejąco', should call selectCurrentPage(1)", async () => {          
+        const {container, getByLabelText} = renderComponent()
+        const sortingDirectionButton = getByLabelText("Zmień kierunek sortowania")
+
+        await user.click(sortingDirectionButton)
+        
+        jest.clearAllMocks()
+        
+        await user.click(sortingDirectionButton)
+
+        expect(container).toMatchSnapshot()
+        expect(mockSelectCurrentPage).toHaveBeenCalledWith(1)
     })
 })
