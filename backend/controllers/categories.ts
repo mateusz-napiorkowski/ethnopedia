120e4ce3caf1159f2b3c483c23392b0d307c9c4e
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2025-05-12 17:04:12 +0200

    add validation and fix tests for /controllers/categories

--- backend/controllers/categories.ts
+++ backend/controllers/categories.ts
@@ -4,12 +4,16 @@ import { getAllCategories } from "../utils/categories"
 export const getCategories = async (req: Request, res: Response) => {
     try {
         const collectionIds = req.query.collectionIds as Array<string>
+        if(!collectionIds)
+            throw new Error("Request is missing query params")
         const categories = await getAllCategories(collectionIds)
         res.status(200).json({categories: categories})
     } catch (error) {
         const err = error as Error
         console.error(error)
-        if (err.message === `Collection not found`)
+        if (err.message === `Request is missing query params`)
+            res.status(400).json({ error: err.message })
+        else if (err.message === `Collection not found`)
             res.status(404).json({ error: err.message })
         else
             res.status(503).json({ error: `Database unavailable` })
