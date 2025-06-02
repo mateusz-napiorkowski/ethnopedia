5c78811008a85c96338e157e9ea3455470f48ccf
mateusz-napiorkowski <mateusz.napiorkowski0@gmail.com>
2025-06-02 09:30:46 +0200

    refactor and test /src/api/dataExport

---
+++ frontend/src/api/__tests__/dataExport.test.ts
@@ -0,0 +1,82 @@
+import '@testing-library/jest-dom';
+import {getXlsxWithArtworksData, getXlsxWithCollectionData} from '../dataExport';
+import axios from "axios"
+import 'dotenv/config'
+import { collectionId, axiosError } from './utils/consts';
+import {ExportExtent} from "../../@types/DataExport"
+
+jest.mock("axios");
+const mockAxios = axios as jest.Mocked<typeof axios>;
+
+(global.URL.createObjectURL as jest.Mock) = jest.fn(() => 'blob:http://localhost/fake-blob-url');
+(global.URL.revokeObjectURL as jest.Mock) = jest.fn();
+
+describe("dataExport tests", () => {
+    beforeEach(() => {   
+        jest.clearAllMocks()
+    });
+
+    describe("getXlsxWithArtworksData tests", () => {
+        it("should call axios.get with correct parameters", async () => {
+            mockAxios.get.mockResolvedValueOnce({  });
+
+            const result = await getXlsxWithArtworksData(
+                [collectionId],
+                ["Tytuł"],
+                ExportExtent.all,
+                {},
+                new URLSearchParams(),
+                "test.xlsx"
+            );
+
+
+            expect(mockAxios.get).toHaveBeenCalledWith(
+                `${process.env.REACT_APP_API_URL}v1/dataExport`,
+                {
+                    responseType: "blob",
+                    params: {
+                        collectionIds: [collectionId],
+                        columnNames: ["Tytuł"],
+                        exportExtent: "all",  
+                        selectedArtworks: []
+                    },
+                    
+                }
+            )
+        });
+
+        it("should throw error if API call fails", async () => {
+            mockAxios.get.mockRejectedValueOnce(new Error("Network Error"));
+
+            await expect(getXlsxWithArtworksData(
+                [collectionId],
+                ["Tytuł"],
+                ExportExtent.all,
+                {},
+                new URLSearchParams(),
+                "test.xlsx"
+            )).rejects.toThrow(axiosError);
+        });
+    })
+
+    describe("getXlsxWithCollectionData tests", () => {
+        it("should call axios.get with correct parameters", async () => {
+            mockAxios.get.mockResolvedValueOnce({  });
+
+            const result = await getXlsxWithCollectionData(collectionId);
+
+            expect(mockAxios.get).toHaveBeenCalledWith(
+                `${process.env.REACT_APP_API_URL}v1/dataExport/collection/${collectionId}`,
+                {
+                    responseType: "blob",                    
+                }
+            )
+        });
+
+        it("should throw error if API call fails", async () => {
+            mockAxios.get.mockRejectedValueOnce(new Error("Network Error"));
+
+            await expect(getXlsxWithCollectionData(collectionId)).rejects.toThrow(axiosError);
+        });
+    })
+})
