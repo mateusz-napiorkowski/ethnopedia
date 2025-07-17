4d7ae95ad1d7ad0c842c1a2fc3fab0d25c71caee
Wojciech Kasperski <wojciech.kasperski@put.poznan.pl>
2025-07-18 00:29:55 +0200

    Add mongo connection logs

--- backend/db/connect.ts
+++ backend/db/connect.ts
@@ -1,7 +1,15 @@
 import mongoose from "mongoose"
 
-const connectDB = (url: any) => {
-    return mongoose.connect(url ?? "mongodb://")
-}
+const connectDB = async (uri: string) => {
+    try {
+        await mongoose.connect(uri ?? "mongodb://", {
+            serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1);
+    }
+};
 
 export default connectDB
