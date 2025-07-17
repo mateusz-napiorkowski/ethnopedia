f7473d8d1e6878027986c6f3b163079de0f48fc9
Wojciech Kasperski <wojciech.kasperski@put.poznan.pl>
2025-07-18 00:34:17 +0200

    Add mongo connection logs & env for mongo uri

--- backend/db/connect.ts
+++ backend/db/connect.ts
@@ -1,8 +1,12 @@
 import mongoose from "mongoose"
 
 const connectDB = async (uri: string) => {
+    if (!uri) {
+        throw new Error("MONGO_URI is not defined");
+    }
+
     try {
-        await mongoose.connect(uri ?? "mongodb://", {
+        await mongoose.connect(uri, {
             serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
         });
         console.log("MongoDB connected successfully");
